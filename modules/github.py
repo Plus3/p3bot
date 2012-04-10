5f613f454a753849c7e0b4a99e37ef0afe186420
Andrei Z <b1naryth1ef@gmail.com>
2012-04-10 02:21:26 -0500

    cont

--- modules/github.py
+++ modules/github.py
@@ -2,32 +2,34 @@ from lib import Command, Event
 from mod_data import githubkey
 from github2.client import Github
 import os, sys, time
-from lib import app
+import socket
 
 channel = '#urtdevs'
 CLI = Github(username="B1narysB0t", api_token=githubkey)
 REPOS = [('urtdevs/website', 'master')]
 REPOSS = []
 
-@app.route("/helo_api/")
-def smsEcho():
-    print request.args
 
-def init(): pass
+def init():
+    for pos, i in enumerate(REPOS):
+        REPOSS.append([CLI.repos.show(i[0])])
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Please dont jizz in this sock plz ty
+    sock.bind(("127.0.0.1", 5050))
+    sock.listen(1)
+    while running: #Not sure if this will loop even when parsing; inspect later
+        client, address = sock.accept()
+        data = client.recv(2048) #2048 can be bumped up later, not really a problem
+        print data
+
 def ready(c): pass
-    
-    # for pos, i in enumerate(REPOS):
-    #     REPOSS.append([CLI.repos.show(i[0])])
-
-# def ready(c):
-#     while True:
-#         #print 'Looping'
-#         for pos, i in enumerate(REPOS):
-#             z = CLI.repos.show(i[0])
-#             #print z.pushed_at, REPOSS[pos][0].pushed_at
-#             if z.pushed_at != REPOSS[pos][0].pushed_at:
-#                 REPOSS[pos][0] = z
-#                 m = CLI.commits.list(i[0], i[1])
-#                 #print 'Sending...'
-#                 c.send(channel, 'New commit on %s [%s]: %s (%s)' % (i[0], i[1], m[0].message, 'http://github.com'+m[0].url))
-#         time.sleep(30)
+    # while True:
+    #     #print 'Looping'
+    #     for pos, i in enumerate(REPOS):
+    #         z = CLI.repos.show(i[0])
+    #         #print z.pushed_at, REPOSS[pos][0].pushed_at
+    #         if z.pushed_at != REPOSS[pos][0].pushed_at:
+    #             REPOSS[pos][0] = z
+    #             m = CLI.commits.list(i[0], i[1])
+    #             #print 'Sending...'
+    #             c.send(channel, 'New commit on %s [%s]: %s (%s)' % (i[0], i[1], m[0].message, 'http://github.com'+m[0].url))
+    #     time.sleep(15)
